9a0b80a52ec511c7c2e9d535ba6d5ab04546da38
Nathanael <nathanael.pivot@ynov.com>
2023-09-24 19:46:15 +0200

    tuto V1

--- exec/main.go
+++ exec/main.go
@@ -4,7 +4,13 @@ import p "RED"
 
 func main() {
 	p.P1.CharCreation()
+	//p.P1.Intro()
 	p.M1.InitGobelin()
-	p.P1.TPT(&p.M1)
-	p.Menu()
+	p.P1.TPTtuto(&p.M1)
 }
+
+/*
+p.P1.TPT(&p.M1)
+	p.M2.InitGobelin()
+	p.Menu()
+*/
